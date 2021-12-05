7b6f3b5fbdafad9a7c6494bca02fd0da7184de75
eigeneddie <66312407+eigeneddie@users.noreply.github.com>
2021-12-05 15:59:09 +0700

    added windows procedure function

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,16 +1,55 @@
 #include <windows.h>
 
-int CALLBACK WinMain(
-  HINSTANCE hInstance,
-  HINSTANCE hPrevInstance,
-  LPSTR     lpCmdLine,
-  int       nShowCmd)
+LRESULT CALLBACK 
+MainWindowCallBack(HWND   Window, //opaque piece of data
+                   UINT   Message,//message to handle
+                   WPARAM WParam, //
+                   LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+  switch(Message)
+  {
+    case WM_SIZE:
+    {
+      OutputDebugStringA("WM_SIZE\n");  
+    }break;
+    case WM_DESTROY:
+    {      
+        OutputDebugStringA("WM_DESTROY\n");  
+    } break;
+    case WM_CLOSE:
+    {
+      OutputDebugStringA("WM_CLOSE\n");  
+    }break;
+    
+    case WM_ACTIVATEAPP:
+    {
+      OutputDebugStringA("WM_ACTIVATEAPP\n");  
+    }break;
+    
+    default:
+    {
+      //OutputDebugStringA("default\n");  
+      Result = ;
+    }break;
+  }
+  return(Result);
+}
+
+
+int CALLBACK 
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR     CommandLine,
+        int       ShowCode)
 {
 
+    //TODO: check if hredraw and other stuff is relevant
     WNDCLASS WindowClass = {};
-    WindowClass.style = CS_OWNDC;
-    WindowClass.lpfnWndProc = ;
-    WindowClass.hInstance = hInstance;
+    WindowClass.style = CS_OWNDC | CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallBack;
+    WindowClass.hInstance = Instance;
     //WindowClass.hIcon;
     WindowClass.lpszClassName = "HandmadeHeroWindowClass";
 
@@ -21,4 +60,11 @@ int CALLBACK WinMain(
 /*
     MessageBoxA(0, "This is handmadehero", "Handmade Hero", 
                 MB_OK|MB_ICONINFORMATION);
+*/
+
+/*
+Minute Notes
+
+EP2:
+-always refer to the MSDN for windows interfacing: GET USED TO IT
 */
